53c41729d97c09206597e653bc0aa48a8d6d243b
paulitina <langeely@mail.ru>
2021-05-28 20:07:36 +0300

    Refactoring - removed boilerplate code

--- src/hw5/TablesToCompare.java
+++ src/hw5/TablesToCompare.java
@@ -1,22 +1,14 @@
 package hw5;
 
-import com.sun.javafx.collections.MappingChange;
-
 import java.sql.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TablesToCompare {
-    public static Connection connection1 = null;
-    public static Connection connection2 = null;
-    public static Statement statement1;
-    public static Statement statement2;
-    public static ResultSet resultSet1;
-    public static ResultSet resultSet2;
-    public static List<Map<String, String>> listOfMapsForDB1 = new ArrayList<>();
-    public static List<Map<String, String>> listOfMapsForDB2 = new ArrayList<>();
-
     private static class MapEntry {
         private String key;
         private String value;
@@ -43,8 +35,50 @@ public class TablesToCompare {
         }
     }
 
-    private static List<Map<String, String>> addResultSetToList(int rowCount, ResultSet resultSet, String... columns) {
-       return IntStream.range(0, rowCount)
+
+    public static void main(String[] args) {
+        try (Connection connection1 = createConnection("jdbc:postgresql://localhost:5432/db1_hw5", "postgres", "postgres");
+             Connection connection2 = createConnection("jdbc:postgresql://localhost:5432/db2_hw5", "postgres", "postgres");
+             Statement statement1 = connection1.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+             Statement statement2 = connection2.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
+
+            compareTables(statement1, statement2, "id", "first_name", "last_name");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static Connection createConnection(String jdbcUrl,
+                                               String username,
+                                               String password) throws SQLException {
+        Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+        if (connection == null) {
+            throw new RuntimeException("No DB connection");
+        }
+        return connection;
+    }
+
+
+    public static void compareTables(Statement statement1,
+                                     Statement statement2,
+                                     String... columns) throws SQLException {
+        try (ResultSet resultSet1 = statement1.executeQuery("SELECT * FROM users");
+             ResultSet resultSet2 = statement2.executeQuery("SELECT * FROM users")) {
+
+            List<Map<String, String>> listOfMapsForDB1 = createListFromResultSet(resultSet1, columns);
+            List<Map<String, String>> listOfMapsForDB2 = createListFromResultSet(resultSet2, columns);
+
+            System.out.println("Значения таблиц по введенным столбцам равны? " + listOfMapsForDB1.equals(listOfMapsForDB2));
+        }
+    }
+
+
+    private static List<Map<String, String>> createListFromResultSet(ResultSet resultSet,
+                                                                     String... columns) throws SQLException {
+        int rowCount = getRowCount(resultSet);
+        return IntStream.range(0, rowCount)
                 .mapToObj(i -> {
                             try {
                                 if (resultSet.next()) {
@@ -71,111 +105,12 @@ public class TablesToCompare {
                 .collect(Collectors.toList());
     }
 
-//    private static List<Map<String, String>> addResultSetToList(int rowCount, ResultSet resultSet, String... columns) {
-//        return IntStream.range(0, rowCount)
-//                .mapToObj(i -> {
-//                                if (resultSet.next()) {
-//                                    return Arrays.stream(columns)
-//                                            .map(columnName -> {
-//                                                    return new MapEntry(columnName, resultSet.getString(columnName));
-//                                            })
-//                                            .filter(Objects::nonNull)
-//                                            .collect(Collectors.groupingBy(MapEntry::getKey));
-//                                }
-//                        }
-//                )
-//                .filter(Objects::nonNull)
-//                .collect(Collectors.toList());
-//    }
 
-    private static int getRowCount(ResultSet resultSet){
-        try{
-            resultSet.last();
-            int rowCount = resultSet.getRow();
-            resultSet.beforeFirst();
-            return rowCount;
-        }catch (SQLException e){
-            e.printStackTrace();
-            return 0;
-        }
-    }
-
-    public static void compareTables(String... columns) {
-        try {
-            resultSet1 = statement1.executeQuery("SELECT * FROM users");
-            resultSet2 = statement2.executeQuery("SELECT * FROM users");
-            int rowCount1 = getRowCount(resultSet1);
-            int rowCount2 = getRowCount(resultSet2);
-            listOfMapsForDB1 = addResultSetToList(rowCount1, resultSet1, columns);
-            listOfMapsForDB2 = addResultSetToList(rowCount2, resultSet2, columns);
-            System.out.println("Значения таблиц по введенным столбцам равны? " +
-                    listOfMapsForDB1.equals(listOfMapsForDB2));
-            statement1.close();
-            statement2.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (resultSet1 != null) {
-                    resultSet1.close();
-                }
-            } catch (Exception e) {
-            }
-            try {
-                if (resultSet2 != null) {
-                    resultSet2.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
-    public static void main(String[] args) {
-        try {
-            connection1 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/db1_hw5",
-                    "postgres", "postgres");
-            if (connection1 == null) {
-                System.out.println("No DB connection");
-                System.exit(0);
-            }
-            statement1 = connection1.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            connection2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/db2_hw5",
-                    "postgres", "postgres");
-            if (connection2 == null) {
-                System.out.println("No DB connection");
-                System.exit(0);
-            }
-            statement2 = connection2.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            compareTables("id", "first_name", "last_name");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (connection1 != null) {
-                    connection1.close();
-                }
-            } catch (SQLException e) {
-            }
-            try {
-                if (connection2 != null) {
-                    connection2.close();
-                }
-            } catch (SQLException e) {
-            }
-            try {
-                if (statement1 != null) {
-                    statement1.close();
-                }
-            } catch (Exception e) {
-            }
-            try {
-                if (statement2 != null) {
-                    statement2.close();
-                }
-            } catch (Exception e) {
-            }
-        }
+    private static int getRowCount(ResultSet resultSet) throws SQLException {
+        resultSet.last();
+        int rowCount = resultSet.getRow();
+        // have to do this to return the cursor to start position
+        resultSet.beforeFirst();
+        return rowCount;
     }
 }
